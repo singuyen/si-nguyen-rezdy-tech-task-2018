a267ae5acbd2c930b63790af4370ba2ab4ad781a
Si Nguyen <nguyensi.art@gmail.com>
2018-10-02 17:36:34 +1000

    Fixed store/index path

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { getters } from 'getters'
-import { actions } from 'actions'
+import { getters } from './getters'
+import { actions } from './actions'
 
 Vue.use(Vuex)
 
